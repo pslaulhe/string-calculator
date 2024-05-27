1e5e58488e4e424c1fcd29c6c015508cb1dd71de
pslaulhe <pslaulhe@gmail.com>
2024-05-27 09:22:12 +0200

    "1,2" returns 3

--- StringCalculator.test.ts
+++ StringCalculator.test.ts
@@ -8,4 +8,8 @@ describe('StringCalculator', () => {
     it('should return 1 for "1"', () => {
         expect(StringCalculator.Add('1')).toBe(1);
     });
+
+    it('should return 3 for "1,2"', () => {
+        expect(StringCalculator.Add('1,2')).toBe(3);
+    });
 });
