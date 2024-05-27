01cf2f418f544f6486ca424f668cad5f0e6f4dd9
pslaulhe <pslaulhe@gmail.com>
2024-05-27 11:26:22 +0200

    Two numbers REFACTOR 3

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -1,10 +1,8 @@
 
 export class StringCalculator {
     static Add(numbers: string): number {
-        if (numbers === '1,4') return parseInt(numbers.split(',')[0]) + parseInt(numbers.split(',')[1]);
-        if (numbers === '4,5') return parseInt(numbers.split(',')[0]) + parseInt(numbers.split(',')[1]);
-        if (numbers === '1,2') return parseInt(numbers.split(',')[0]) + parseInt(numbers.split(',')[1]);
         if (numbers === '1') return 1;
-        return 0;
+        if (numbers === '') return 0;
+        else return parseInt(numbers.split(',')[0]) + parseInt(numbers.split(',')[1]);
     }
 }
