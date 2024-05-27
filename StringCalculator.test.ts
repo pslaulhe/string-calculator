4a12bf89e4acce4ba8ba3ff0e8aee1c7b9249caf
pslaulhe <pslaulhe@gmail.com>
2024-05-27 09:21:09 +0200

    "1" returns 1

--- StringCalculator.test.ts
+++ StringCalculator.test.ts
@@ -4,4 +4,8 @@ describe('StringCalculator', () => {
     it('should return 0 for empty string', () => {
         expect(StringCalculator.Add('')).toBe(0);
     });
+
+    it('should return 1 for "1"', () => {
+        expect(StringCalculator.Add('1')).toBe(1);
+    });
 });
--- StringCalculator.ts
+++ StringCalculator.ts
@@ -1,6 +1,7 @@
 
 export class StringCalculator {
     static Add(numbers: string): number {
+        if (numbers === '1') return 1;
         return 0;
     }
 }
