41695d1333cef3073e94f54bb8c751cca944c17b
pslaulhe <pslaulhe@gmail.com>
2024-05-28 08:42:02 +0200

    One number Refactor 1

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -1,9 +1,9 @@
 
 export class StringCalculator {
     static Add(numbers: string): number {
-        if (numbers === '1') return 1;
-        if (numbers === '4') return 4;
-        if (numbers === '5') return 5;
+        if (numbers === '1') return parseInt(numbers.split('.')[0]);
+        if (numbers === '4') return parseInt(numbers.split('.')[0]);
+        if (numbers === '5') return parseInt(numbers.split('.')[0]);
         if (numbers === '') return 0;
         else return parseInt(numbers.split(',')[0]) + parseInt(numbers.split(',')[1]);
     }
