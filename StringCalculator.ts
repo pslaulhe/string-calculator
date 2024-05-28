8a5a82473f4d7d549a25f9daf4c28218e4619267
pslaulhe <pslaulhe@gmail.com>
2024-05-28 09:13:17 +0200

    Handle different separators GREEN

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -1,6 +1,12 @@
 
 export class StringCalculator {
     static Add(numbers: string): number {
+        if (numbers.startsWith('//')) {
+            const separator = numbers[2];
+            numbers = numbers.substring(4);
+            numbers = numbers.replace(separator, ',');
+        }
+
         numbers = numbers.replace('\n', ',');
         const numbersArray = numbers.split(',').map(n => parseInt(n));
         if (numbers === '') return 0;
