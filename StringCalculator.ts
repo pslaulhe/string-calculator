d7f7093dc142be9fde60f42540ae9a2c1774e154
pslaulhe <pslaulhe@gmail.com>
2024-05-28 09:09:00 +0200

    Handle new lines separator GREEN

--- StringCalculator.ts
+++ StringCalculator.ts
@@ -1,6 +1,7 @@
 
 export class StringCalculator {
     static Add(numbers: string): number {
+        numbers = numbers.replace('\n', ',');
         const numbersArray = numbers.split(',').map(n => parseInt(n));
         if (numbers === '') return 0;
         else return numbersArray.reduce((a, b) => a + b);
